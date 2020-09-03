b23e986fbc38275bee4663011269980691799c45
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-03 20:37:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,7 @@
-def reverse_each_word(string)
-  new = string.split(" ")
-  new1 = []
-  new.each do |x|
-    new1 << x.reverse
-  end
-  new1.join(" ")
+def reverse_each_word(str)
+  str.split.collect {|x| x.reverse}.join(" ")
+  
 end
-
-puts reverse_each_word("Hello there, and how are you?")
 
 # total = 0
 # [1,2,3,4].each do |num|
